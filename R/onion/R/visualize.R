365551c0b8f7534b611e2deb83a617c5328ceafd
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2015-11-29 21:51:30 +0100

    Removed Includes as that should be taken care of by the namespace mechanism

--- R/onion/R/visualize.R
+++ R/onion/R/visualize.R
@@ -1,13 +1,12 @@
 #'@export
+#'@import ggplot2
 plotospace <- function(data,ospace) {
-  library(ggplot2)
   newdata <- data.frame(data,ospace$assignment)
   ggplot(newdata,aes(x=Sales,y=CompetitionDistance,color=ospace.assignment)) + 
     geom_point(size=1.3) 
 }
 #'@export
 plotassnvec <- function(data,assnvec) {
-  library(ggplot2)
   newdata <- data.frame(data,assnvec)
   ggplot(newdata,aes(x=Sales,y=CompetitionDistance,color=assnvec)) + 
     geom_point(size=1.3) 
