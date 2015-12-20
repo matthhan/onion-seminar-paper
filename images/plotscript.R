92b83d8f9dc9b8c242b11186d2c73e6745077977
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2015-12-20 15:07:54 +0100

    wrote script to plot part of the results

---
+++ images/plotscript.R
@@ -0,0 +1,28 @@
+data <- read.csv("../results/vary_dataset_size.csv",header=T,sep=",")
+library("ggplot2")
+#ospace construction compared to knn query
+ggplot(data,aes(x=dataset_size)) + 
+  geom_line(aes(y=knn_time,linetype="knn_time")) + geom_point(aes(y=knn_time))+
+  geom_line(aes(y=ospace_time,linetype="ospace_time")) + geom_point(aes(y=ospace_time)) + 
+  guides(linetype=guide_legend(title="Processing Time"))+ 
+  scale_linetype_discrete(labels=c("K-Nearest-Neighbors Query","O-Space Construction Overhead"))+ 
+  labs(x="Dataset Size (Pairs)",y="Time (Seconds)")
+#space constructions over dataset size
+ggplot(data,aes(x=dataset_size)) +
+  geom_line(aes(y=ospace_time,linetype="ospace_time")) + geom_point(aes(y=ospace_time)) +
+  geom_line(aes(y=pspace_time,linetype="pspace_time")) + geom_point(aes(y=pspace_time)) +
+  geom_line(aes(y=dspace_time,linetype="dspace_time")) + geom_point(aes(y=dspace_time)) +
+  guides(linetype=guide_legend(title="Processing Time"))+ 
+  scale_linetype_discrete(labels=c("D-Space Construction","O-Space Construction","P-Space Construction"))+ 
+  labs(x="Dataset Size (Pairs)",y="Time (Seconds)")
+#dspace and pspace construction over number of outlier candidates
+ggplot(data,aes(x=outlier_candidates)) +
+  geom_line(aes(y=pspace_time,linetype="pspace_time")) + geom_point(aes(y=pspace_time)) +
+  geom_line(aes(y=dspace_time,linetype="dspace_time")) + geom_point(aes(y=dspace_time)) +
+  guides(linetype=guide_legend(title="Processing Time"))+ 
+  scale_linetype_discrete(labels=c("D-Space Construction","P-Space Construction"))+ 
+  labs(x="Number of Outlier Candidates",y="Time (Seconds)")
+#Number of outliers dataset size
+ggplot(data,aes(x=dataset_size)) +
+  geom_point(aes(y=outlier_candidates))+
+  labs(x="Dataset Size",y="Number of Outlier Candidates")
