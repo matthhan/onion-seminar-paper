4cb714cef2343f477c232b00eab7b589d4634480
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2015-12-20 15:08:47 +0100

    miscellaneous tiny changes while testing

--- R/preprocess_osmdata.R
+++ R/preprocess_osmdata.R
@@ -1,5 +1,5 @@
 load("..//data//south_america_osm_data.Rdata")
-data <- south_america[sample(1:nrow(south_america),nrow(south_america) * 0.5),]
+data <- south_america[sample(1:nrow(south_america),10000000),]
 rm(south_america)
 gc()
 
