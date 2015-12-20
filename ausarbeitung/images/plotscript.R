3cba3093074325d3f03ea4691376bb3db1336405
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2015-12-20 17:43:32 +0100

    added two more plots

--- ausarbeitung/images/plotscript.R
+++ ausarbeitung/images/plotscript.R
@@ -1,4 +1,4 @@
-data <- read.csv("../results/vary_dataset_size.csv",header=T,sep=",")
+data <- read.csv("../../results/vary_dataset_size.csv",header=T,sep=",")
 library("ggplot2")
 #ospace construction compared to knn query
 ggplot(data,aes(x=dataset_size)) + 
@@ -26,3 +26,29 @@ ggplot(data,aes(x=outlier_candidates)) +
 ggplot(data,aes(x=dataset_size)) +
   geom_point(aes(y=outlier_candidates))+
   labs(x="Dataset Size",y="Number of Outlier Candidates")
+#read dataset on pspace and dspace durations by outlier candidates
+data <- read.csv("../../results/vary_parameters_outlier_candidates.csv",header=T,sep=",")
+ggplot(data,aes(x=outlier_candidates)) + 
+  geom_line(aes(y=dspace_construction)) + geom_point(aes(y=dspace_construction)) + 
+  scale_y_log10(breaks=c(1,10,100)) +
+  labs(y="Time (Seconds)",x="Number of Outlier Candidates")
+data <- read.csv("../../results/vary_k_max.csv",header=T,sep=",")
+ggplot(data,aes(x=k_max)) + 
+  geom_line(aes(y=duration_knn_query)) + geom_point(aes(y=duration_knn_query)) + 
+  labs(y="Time (Seconds)",x="Value for kmax") + expand_limits(y=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
